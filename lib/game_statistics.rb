aad7bff7fb377bec4cd16a7471a10d1be39678ff
Hoa Dam <hoangocdam@gmail.com>
2024-01-14 21:22:56 -0800

    fix: pct rounding game_stats

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -14,14 +14,14 @@ class GameStatistics
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
       games << Game.new(row)
     end
-    
+
     new(games)
   end
 
   def highest_total_score
     highest_score = 0
-    @games.each do |game| 
-      total_score = game.home_goals + game.away_goals 
+    @games.each do |game|
+      total_score = game.home_goals + game.away_goals
       highest_score = total_score if total_score > highest_score
     end
     highest_score
@@ -29,29 +29,29 @@ class GameStatistics
 
   def lowest_total_score
     lowest_score = Float::INFINITY
-    @games.each do |game| 
-      total_score = game.home_goals + game.away_goals 
+    @games.each do |game|
+      total_score = game.home_goals + game.away_goals
       lowest_score = total_score if total_score < lowest_score
     end
     lowest_score == Float::INFINITY ? 0 : lowest_score
-  end 
+  end
 
-  def percentage_home_wins 
-    total_games = @games.size 
+  def percentage_home_wins
+    total_games = @games.size
     home_wins = @games.count { |game| game.home_goals > game.away_goals }
-    (home_wins.to_f / total_games * 100).round(2)
+    (home_wins.to_f / total_games).round(2)
   end
 
   def percentage_away_wins
-    total_games = @games.size 
+    total_games = @games.size
     away_wins = @games.count { |game| game.away_goals > game.home_goals }
-    (away_wins.to_f / total_games * 100).round(2)
+    (away_wins.to_f / total_games).round(2)
   end
 
   def percentage_ties
-    total_games = @games.size 
+    total_games = @games.size
     tie = @games.count { |game| game.away_goals == game.home_goals }
-    (tie.to_f / total_games * 100).round(2)
+    (tie.to_f / total_games).round(2)
   end
 
   def count_of_games_by_season
@@ -92,4 +92,3 @@ class GameStatistics
   end
 
 end
-
